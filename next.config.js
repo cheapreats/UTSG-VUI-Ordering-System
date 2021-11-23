35f2504bc0f6357ad27150f3c2c030fbf451c710
frootloopers <greenghost96@gmail.com>
2021-11-23 00:53:19 -0500

    added cheapreats stuff

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/oauth",
+        has: [
+          {
+            type: "query",
+            key: "code",
+          },
+        ],
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+
+  webpack: (config, options) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+      "styled-components": path.resolve("./node_modules/styled-components"),
+    };
+    return config;
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Global, ThemeTypes } from "@cheapreats/react-ui"
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Global theme={ThemeTypes.MAIN}>
+      <Component {...pageProps} />
+    </Global>
+  );
 }
 
 export default MyApp
