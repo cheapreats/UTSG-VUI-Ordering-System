73f5f0004cc313291a683c6b8737fbde658e1bbf
Supanat Wangsutthitham <soup.wangsutthitham@mail.utoronto.ca>
2021-11-23 00:25:10 -0500

    feat: cleared old stuffs and initialized nextjs app

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# UTSG-VUI-Ordering-System
--- src/pages/Page1.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export const Page1 = (): React.ReactElement => {
-  return (
-    <>
-      <Link to="/">Back to home</Link>
-    </>
-  );
-};
