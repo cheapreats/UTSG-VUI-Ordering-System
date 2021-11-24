23a1dfdfccb6848cc0ddf11cbf7a72b024dc9976
Supanat Wangsutthitham <soup.wangsutthitham@mail.utoronto.ca>
2021-11-23 21:11:40 -0500

    fix: exported default checkout & commented button

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -29,3 +29,5 @@ export const Checkout = (): React.ReactElement => {
     </>
   );
 };
+
+export default Checkout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
-import type { NextPage } from "next"
-import Link from "next/link"
-import Head from "next/head"
-import Image from "next/image"
-import { Button } from "@cheapreats/react-ui"
+import type { NextPage } from "next";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import { Button } from "@cheapreats/react-ui";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Button>
-        Welcome to Home
-      </Button>
+      <Button>Welcome to Home</Button>
       <Link href="/test">To Test</Link>
     </div>
   );
