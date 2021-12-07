47e7c93647ef5b72407a46e78c9d39e760550c9f
Khatchig Anteblian <ainteblian99@gmail.com>
2021-12-07 12:57:39 -0500

    implemented home page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,128 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
-import { Button } from "@cheapreats/react-ui";
+import styled from "styled-components";
+import { Button, Heading, CarouselTestimonial } from "@cheapreats/react-ui";
+import { BagFill, InfoCircleFill } from "@styled-icons/bootstrap";
+import { Parallax } from "react-parallax";
+import { useRef } from "react";
+
+
+const LOGO = "https://www.cheapreats.com/static/90939a6dc8dacea8e44d046c72521a1b/16c7d/logo.png";
+const IMAGE = "https://images.unsplash.com/photo-1604573714289-312a6972f67c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80";
+
+const CAROUSELCONF = {
+  title: "",
+  image: LOGO,
+  reviews: [
+    {testimony: "testimony one", reviewer: "rando one"},
+    {testimony: "testimony two", reviewer: "rando two"},
+    {testimony: "testimony three", reviewer: "rando three"}
+  ],
+  autoPlay: true,
+  interval: 5000,
+  loop: true
+}
+
+const COLORS = {
+  primary: "#EE2434",
+  text: "#4a4a4a"
+}
+
+
+export const redirectToOrder = () => {
+  console.log("redirect");
+  window.location.replace("/order");
+}
+
+const scrollOnClick = () => {
+  console.log("Start scrolling");
+}
+
 
 const Home: NextPage = () => {
+
+  const scrollRef = useRef();
+
+  const scrollOnClick = () => {
+    scrollRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div>
-      <Link href="/landing">To Landing</Link>
+      <Parallax
+        blur={{min: -8, max: 8}}
+        bgImage={ IMAGE } 
+        bgStyle={{
+          backgroundSize: "cover"
+        }}
+        strength={300}>
+        <Section>
+          <Container>
+            <Heading margin="10px" color="black" type="h1" size="3rem" textAlign="center" bold={true} children="Welcome to our Voice Ordering System" lineHeight="1.3" />
+            <Button onClick={redirectToOrder} icon={BagFill} children="Start Ordering" primary={true} iconSize="20px" margin="5px" />
+            <Button onClick={scrollOnClick} icon={InfoCircleFill} children="Learn More" iconSize="20px" margin="5px" />
+          </Container>
+        </Section>
+      </Parallax>
+      <Section>
+        <Subsection>
+          <Img src={ LOGO } />
+        </Subsection>
+        <Subsection bgCol={ COLORS.primary }>
+          <Heading type="h1" color="white" bold={true} children="How it works" />
+          <h1 ref={scrollRef}> </h1>
+          <CarouselTestimonial 
+            carouselTitle={ CAROUSELCONF.title }
+            carouselImage={ CAROUSELCONF.image }
+            reviews={ CAROUSELCONF.reviews }
+            isAutoplaying={ CAROUSELCONF.autoPlay }
+            carouselInterval={ CAROUSELCONF.interval }
+            isLooping={ CAROUSELCONF.loop }
+          />
+          <Button onClick={redirectToOrder} icon={BagFill} children="Start Ordering" iconSize="20px" margin="5px" />
+        </Subsection>
+      </Section>
     </div>
   );
 };
+
+const Img = styled.img`
+  width: 25%;
+  height: 25%;
+  margin: 15px;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+  height: 50%;
+  border-radius: 15px;
+  background-color: white;
+`
+
+const Section = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  width: 100%;
+  height: 100vh;
+`
+
+const Subsection = styled.div<{bgCol?: string}>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  width: 50%;
+  height: 100vh;
+  background-color: ${p => p.bgCol || "#FFF"};
+`
+
 
 export default Home;
